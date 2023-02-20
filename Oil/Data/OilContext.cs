79926e5bdf92b24138aeb280ce3d89fb2983936d
Ataki_pw11 <Ataki_pw11@STHQ012B-05>
2023-02-20 17:10:34 +0400

    View added

---
+++ Oil/Data/OilContext.cs
@@ -0,0 +1,20 @@
+﻿using Oil.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oil.Data
+{
+    public class OilContext: DbContext
+    {
+        public OilContext():
+            base("OilDb")
+        {
+                
+        }
+        public List<OilClass> Oils { get; set; }
+    }
+}
